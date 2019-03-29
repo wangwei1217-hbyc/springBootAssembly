e0037e7fc52df423e07299b6ee036a2814d41154
wangwei <wangwei_hbyc@163.com>
2019-03-29 19:03:52 +0800

    redis

---
+++ src/main/java/com/wangwei/config/RedisConfig.java
@@ -0,0 +1,70 @@
+package com.wangwei.config;
+
+import com.xiaoma.util.RedisTemplate;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.JedisShardInfo;
+import redis.clients.jedis.ShardedJedisPool;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName RedisConfig
+ * @Description Redis 配置类
+ */
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private String port;
+    @Value("${spring.redis.password}")
+    private String password;
+    @Value("${spring.redis.database}")
+    private String database;
+
+    @Bean
+    public JedisShardInfo getJedisShardInfo() {
+        JedisShardInfo jedisShardInfo = new JedisShardInfo(host, port);
+        if(password != null && !"".equals(password)){
+            jedisShardInfo.setPassword(password);
+        }
+        return jedisShardInfo;
+    }
+
+    /**
+     * 自定义redisTemplate
+     * 封装常用redis操作
+     * @return
+     */
+    @Bean(value = "redisTemplate")
+    public RedisTemplate redisTemplate() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxIdle(8);
+        jedisPoolConfig.setTestOnBorrow(true);
+        jedisPoolConfig.setTestOnReturn(true);
+
+        RedisTemplate template = new RedisTemplate();
+        int db = 0;
+        if (StringUtils.isNotEmpty(database)) {
+            db = Integer.valueOf(database);
+        }
+        int p = 6379;
+        if (StringUtils.isNotEmpty(port)) {
+            p = Integer.valueOf(port);
+        }
+        JedisShardInfo shardInfo = template.getJedisShardInfo(host, p, password, db);
+        List<JedisShardInfo> shards = new ArrayList<>();
+        shards.add(shardInfo);
+        ShardedJedisPool shardedJedisPool = new ShardedJedisPool(jedisPoolConfig, shards);
+        template.setShardedJedisPool(shardedJedisPool);
+        return template;
+    }
+
+}
+
--- src/main/java/com/wangwei/controller/TestController.java
+++ src/main/java/com/wangwei/controller/TestController.java
@@ -1,8 +1,11 @@
 package com.wangwei.controller;
 
+import com.xiaoma.util.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * Created by wangwei on 2019/3/28.
@@ -10,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("test")
 public class TestController {
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @GetMapping("index")
     public Object index(){
+        System.out.println(redisTemplate);
         return "success";
     }
 }
